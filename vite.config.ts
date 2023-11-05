63db3712fa928cfc005c5b74ba1b54a5fa70e8f1
= <thorsten.luck@yahoo.de>
2023-11-05 15:56:49 +0100

    changed base url

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/tearaveler-blog/',
+  base: './',
   plugins: [
     vue(),
     vueJsx(),
